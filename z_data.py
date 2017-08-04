471903a5eb7396019c47b01a7e7f3ee02668ae57
yangchuanyun <704746345@qq.com>
2017-08-04 15:03:14 +0800

    2 upload

--- z_data.py
+++ z_data.py
@@ -1,52 +1,67 @@
 # -*-coding:utf-8 -*-
 
 ex_data={
-    "_id" : "595c74ddac72760be08e3902",
-    "resCode" : "0000",
-    "resMsg" : "SUCCESS",
-    "data" : {
-        "resultFlag" : "0",
-        "message" : "查询成功有数据",
-        "result" : [{
-            "phone" : "180XXXX13",
-            "netStatus" : "0",
-            "netStatusMsg" : "在网状态正常"},
-             {
-            "pahone" : "180XXXX13",
-            "anetStatus" : "0",
-            "anetStatusMsg" : "在网状态正常"}
-        ]
-    },
-"aa": [{
-            "phone" : "180XXXX13",
-            "netStatus" : "0",
-            "netStatusMsg" : "在网状态正常"}
-        ],
-    "phone" : "17744561767",
-    "resStatus" : "1",
-    "checkStatus" : "0",
-    "resAdress" : "sjt",
-    "resNum" : "PLCS1116YY006",
-    "resDate" : "2017-07-05 13:09:19",
-    "orderNo" : "20170705130919fc4c68c6",
-    "code" : "111106",
-    "idCard" : "430302197903283058",
-    "accountNo" : "null",
-    "phoneNum" : "17744561767",
-    "userName" : "陈杨名"
-}    
+    "_id":"58eb59e61cfb1e2eca23f222",
+    "resCode":"0000",
+    "resMsg":"SUCCESS",
+    "data":[
+        {
+            "reason":"0",
+            "match":[
+                "national_id",
+                "name",
+                "email"
+            ],
+            "create_date_type":"6",
+            "amount_type":3,
+            "over_due_type":0,
+            "legal_status":0
+        },
+        {
+            "reason":3,
+            "match":[
+                "national_id",
+                "name"
+            ],
+            "create_date_type":0,
+            "amount_type":0,
+            "over_due_type":0,
+            "legal_status":0
+        }
+    ],
+    "name":"黄潮炎",
+    "idCard":"440524197307166614",
+    "createDate":"1491818982193",
+    "flag_view":"1"
+}
 
-ex_data2=[{
-            "phone" : "180XXXX13",
-            "netStatus" : "0",
-            "netStatusMsg" : "在网状态正常"},
-             {
-            "pahone" : [{"mm":"123"},"nn"],
-            "anetStatus" : "0",
-            "anetStatusMsg" : "在网状态正常"},
-"aa",
-"bb"
-        ]
+ex_data2=[
+["ll","oo"],
+
+        {
+            "reason":"0",
+            "match":[
+                "national_id",
+                "name",
+                "email"
+            ],
+            "create_date_type":"6",
+            "amount_type":"3",
+            "over_due_type":"3",
+            "legal_status":"3"
+        },
+        {
+            "reason":"3",
+            "match":[
+                "national_id",
+                "name"
+            ],
+            "create_date_type":"3",
+            "amount_type":"3",
+            "over_due_type":"3",
+            "legal_status":"3"
+        }
+    ]
   
 
 def data_handle(data,jsonpath):   # 第一次调用这个函数的时候，jsonpath 设为 $. 或$
@@ -54,7 +69,7 @@ def data_handle(data,jsonpath):   # 第一次调用这个函数的时候，jsonp
 	if isinstance(data,dict):   #  第一次jsonpath为 $.
 		# print("111")
 		for key in data:
-			if isinstance(data[key],str):
+			if isinstance(data[key],str) or isinstance(data[key],int):
 				# print("22")
 				jsonpath1=jsonpath+key
 				if key!="jsonpath":
@@ -67,10 +82,8 @@ def data_handle(data,jsonpath):   # 第一次调用这个函数的时候，jsonp
 				dict1['jsonpath']=jsonpath1
 				templist.append(dict1)
 			elif isinstance(data[key],list):
-				for each in data[key]:
-					if isinstance(each,dict):
-						jsonpath1=jsonpath+key+'['+str(data[key].index(each))+']'+'.'
-						data_handle(each,jsonpath1)
+				jsonpath1=jsonpath+key
+				data_handle(data[key],jsonpath1)
 	elif isinstance(data,list):   #第一次jsonpath为  $
 		for each in data:
 			if isinstance(each,dict):
@@ -80,16 +93,14 @@ def data_handle(data,jsonpath):   # 第一次调用这个函数的时候，jsonp
 				dic2['jsonpath']=jsonpath1
 				templist.append(dic2)
 			elif isinstance(each,list):
-				for item in each:
-					if isinstance(item,dict):
-						jsonpath1=jsonpath+'['+str(data.index(each))+']'+'['+str(each.index(item))+']'+'.'
-						data_handle(item,jsonpath1)
-			elif isinstance(each,str):
+				jsonpath1=jsonpath+"["+str(data.index(each))+"]"
+				data_handle(each,jsonpath1)
+			elif isinstance(each,str) or isinstance(each,int):
 				jsonpath1=jsonpath+'['+str(data.index(each))+']'
 				if each!="jsonpath":
 					print(jsonpath1)
 	for each in templist:
 		data_handle(each,each['jsonpath'])
 
-data_handle(ex_data2,"$")
+data_handle(ex_data,"$.")
 
